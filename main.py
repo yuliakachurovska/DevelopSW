730fab84c199a0f73e4a3b4d14b041bba75797aa
yuliakachurovska <kachurovska167kv@gmail.com>
2024-02-19 00:01:51 +0200

    new function on the new branch

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from utils import factorial
+from utils import factorial, degree_of_five
 print(factorial(5))
+print(degree_of_five(125))
--- utils.py
+++ utils.py
@@ -11,3 +11,12 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)
+
+def degree_of_five(numb):
+    if numb <= 0:
+        return False
+    elif numb == 1:
+        return True
+    while numb % 5 == 0:
+        numb /= 5
+    return numb == 1
