fa5ca732a15939bfcdb6a93f96b809d57581212a
yuliakachurovska <kachurovska167kv@gmail.com>
2024-02-19 00:06:46 +0200

    added new function

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+<<<<<<< Updated upstream
 from utils import factorial
 print(factorial(5))
+=======
+from utils import factorial, degree_of_five, is_power_of_two
+print(factorial(5))
+print(degree_of_five(125))
+print(is_power_of_two(8))
+>>>>>>> Stashed changes
--- utils.py
+++ utils.py
@@ -11,3 +11,24 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)
+<<<<<<< Updated upstream
+=======
+
+def degree_of_five(numb):
+    if numb <= 0:
+        return False
+    elif numb == 1:
+        return True
+    while numb % 5 == 0:
+        numb /= 5
+    return numb == 1
+
+def is_power_of_two(numb):
+    if numb <= 0:
+        return False
+    elif numb == 1:
+        return True
+    while numb % 2 == 0:
+        numb //= 2
+    return numb == 1
+>>>>>>> Stashed changes
